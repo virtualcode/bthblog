3849f5d109cf95f75da1856db1d4bb96bed1174b
Michael Perry <mperry777@gmail.com>
2023-03-27 18:41:10 -0400

    Making it mine

--- app/elements/site-header.mjs
+++ app/elements/site-header.mjs
@@ -3,11 +3,11 @@ export default function SiteHeader ({ html }) {
     <header>
       <h1 class='text2 text4-lg text-center font-heading font-bold tracking-1 pt4 pt6-lg pb0 pb2-lg'>
         <a href='/' class='no-underline'>
-          Enhance Blog Template
+          Blogging from Beyond The Horizon
         </a>
       </h1>
       <p class='text-center font-body pb4 pb6-lg'>
-        A subtitle for this blog
+        Programming - Web Development - Sailing - Photography
       </p>
     </header>
   `
--- app/head.mjs
+++ app/head.mjs
@@ -1,11 +1,11 @@
 import { getLinkTag } from '@enhance/arc-plugin-styles/get-styles'
 
 export default function Head() {
-  return`
+  return `
     <!DOCTYPE html>
     <html lang='en'>
       <head>
-        <title>Enhance Blog Template</title>
+        <title>BTH Blog</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${getLinkTag()}
         <link rel='stylesheet' href='/_public/css/global.css' />
